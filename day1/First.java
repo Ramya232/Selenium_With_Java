aa32690a9c8f71a1b181da5da9eb116e4187b70a
Ramya232 <nishanth0508@gmail.com>
2025-02-18 04:12:59 +0530

    Add files via upload

---
+++ day1/First.java
@@ -0,0 +1,13 @@
+package weekone.day1;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ramya");
+		
+		
+		
+	}
+
+}
